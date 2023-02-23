86f04be90db081d28431fd974528d34b552b3902
Ziraynov <113580553+Ziraynov@users.noreply.github.com>
2023-02-23 21:31:49 +0300

    Add files via upload

---
+++ src/lab1/Header.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+//void show(monitor* array, int* len);
+//void add(monitor* array, int* len);
+//void stop(monitor* array, int* len);
+//void rem(monitor* array, int* len);
+//void sort(monitor* array, int* len);
+typedef enum {
+	LG, Samsung,GigaByte,AOC, Huawei, Philips, BenQ, HP,Asus
+
+}producer_of_monitor;
+typedef struct {
+	char* name;
+	char* matrix;
+	double price;
+	producer_of_monitor producer;
+}monitor;
+typedef struct {
+	int len;
+	char** str;
+}string;
+void get_key(int* key);
+void find_number(int* number, char* html_string);
+monitor* parse_html(char* path, int* number);
+void show(monitor* array, int len);
+void sort(monitor* arr, int len);
+void add_word(monitor* arr, int* len);
+void rem(monitor* arr, int* len);
+
+
+
+
+
+
+
+char* file(char *path);
+int second_word(const char* pre,const char* string);
+char* get_the_word(char** doc, char* firststr, char* secondstr);
+
+int comp_name(monitor* a, monitor* b);
+
+int comp_price(monitor* a, monitor* b);
+
+int comp_producer(monitor* a, monitor* b);
+
+int comp_producer_matrix(const monitor* a, monitor* b);
+
+int comp_name_price(const monitor* a, const monitor* b);
+
---
+++ src/lab1/main.c
@@ -0,0 +1,28 @@
+﻿#include "Header.h"
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	system("chcp 1251 > nul");
+	setlocale(LC_ALL, ".utf8");
+	const char path[] = { "C:/projects/second term/Lab1/monitors.txt" };
+	int key, number = 0;
+	monitor* arr = parse_html(&path, &number);
+	while (1)
+	{
+		printf("1. Show\n2. Add\n3. Remove\n4. Sort\n5. Exit\n");
+		get_key(&key);
+		switch (key)
+		{
+
+		case 1:show(arr, number); break;
+		case 2:add_word(arr, &number); break;
+		case 3:rem(arr, &number); break;
+		case 4:sort(arr, number); break;
+		case 5:exit(EXIT_SUCCESS);
+		}
+
+	}
+	return 0;
+}
+
